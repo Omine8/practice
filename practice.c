1cc43e0ca4230f45b87f6f90e5f8b35d7848e6a8
Omine <119976886+Omine8@users.noreply.github.com>
2023-11-29 13:06:41 +0530

    Add files via upload

--- practice.c
+++ practice.c
@@ -8,45 +8,46 @@ int total;
 }std[5];
 int main()
 {
-struct StudentDB *ptr;
+struct StudentDB *ptr = (struct StudentDB *)malloc(5 * sizeof(struct StudentDB));
 int sum=0;
 int temp;
 int t;
 for(int i=0;i<5;i++)
 {
-std[i].sno=i+1;
-printf("Enter the Details of Student %d : \n",std[i].sno);
+(*(ptr+i)).sno=i+1;
+printf("Enter the Details of Student %d : \n",(*(ptr+i)).sno);
 for(int j=0;j<5;j++)
 {
 printf("Subject %d : ",j+1);
-scanf("%d",&std[i].marks[j]);
+scanf("%d",&(*(ptr+i)).marks[j]);
 }
 printf("\n");
 for(int j=0;j<5;j++)
 {
-sum = sum + std[i].marks[j];
+sum = sum + (*(ptr+i)).marks[j];
 }
-std[i].total = sum;
+(*(ptr+i)).total = sum;
 sum=0;
 }
 for(int i=0;i<5;i++)
 {
 for(int j=i+1;j<5;j++)
 {
-if(std[i].total<std[j].total)
+if((*(ptr+i)).total<(*(ptr+j)).total)
 {
-temp=std[i].total;
-t=std[i].sno;
-std[i].total=std[j].total;
-std[i].sno = std[j].sno;
-std[j].total=temp;
-std[j].sno=t;
+temp=(*(ptr+i)).total;
+t=(*(ptr+i)).sno;
+(*(ptr+i)).total=(*(ptr+j)).total;
+(*(ptr+i)).sno = (*(ptr+j)).sno;
+(*(ptr+j)).total=temp;
+(*(ptr+j)).sno=t;
 }
 }
 }
 printf("After sorting the students are : \n");
 for(int i=0;i<5;i++)
 {
-printf("Student %d : %d\n",std[i].sno,std[i].total);
+printf("Student %d : %d\n",(*(ptr+i)).sno,(*(ptr+i)).total);
 }
+free(ptr);
 }
